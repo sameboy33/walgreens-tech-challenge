72ad54a80f2f2d77c83bbe2ad9be66bef3db1fbc
sameboy33 <samvitu@MacBook-Pro.local>
2020-08-13 17:55:24 -0400

    Deleted jQuery locally

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
     <link rel="icon" 
       type="image/png" 
       href="res/walgreens-logo.png">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <!-- <script src="jquery-3.5.1.js"></script> -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type='text/javascript' src='config.js'></script>
     <script src="script.js"></script>
     <title>Walgreens Technical Assessment</title>
